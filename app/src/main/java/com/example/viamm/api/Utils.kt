fe55c8f100ef3806faff23a3a39c04bf3cef6215
Artsylapp <artsylapp@gmail.com>
2024-06-03 14:17:25 +0800

    Update Utils.kt

---
+++ app/src/main/java/com/example/viamm/api/Utils.kt
@@ -0,0 +1,7 @@
+package com.example.viamm.api
+
+object Utils {
+    const val BASE_URL = "http://172.20.79.92/Capstoneproject_web/api/" // change IP address to your local or hosting site URL/IP
+
+//    const val BASE_URL = "http://192.168.43.27/Capstoneproject_web/api/" // change IP address to your local or hosting site URL/IP
+}
