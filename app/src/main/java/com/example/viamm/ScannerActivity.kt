a2ad14aa98aefa402e3a27db42b1af0608a849dd
Artsylapp <artsylapp@gmail.com>
2024-06-03 14:44:51 +0800

    merge

---
+++ app/src/main/java/com/example/viamm/ScannerActivity.kt
@@ -0,0 +1,25 @@
+package com.example.viamm
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.TextureView
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viamm.databinding.ActivityMainBinding
+import com.example.viamm.storage.SharedData
+
+class ScannerActivity {
+
+
+
+}
